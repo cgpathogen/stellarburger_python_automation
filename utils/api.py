4226900f1e368a8ea39a298391f452096b056c34
cgpathogen <144326766+cgpathogen@users.noreply.github.com>
2025-03-19 21:47:08 +0300

    URLs were added

---
+++ utils/api.py
@@ -0,0 +1,17 @@
+from utils.http_methods import Http_methods
+
+
+class Stellarburger_api_tests:
+
+    base_url = "https://stellarburgers.nomoreparties.site"
+    ingredients_data = "/api/ingredients"
+    create_order = "/api/orders"
+    password_reset = "/api/password-reset"
+    create_user = "/api/auth/register"
+    auth = "/api/auth/login"
+    register = "/api/auth/register"
+    logout = "/api/auth/logout"
+    refresh_token = "/api/auth/token"
+    user_data = "/api/auth/user"
+    all_orders = "/api/orders/all"
+    user_orders = "/api/orders"
