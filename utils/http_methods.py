05d92c679748cd8af6a97bc6d3508703dc63f204
cgpathogen <144326766+cgpathogen@users.noreply.github.com>
2025-03-19 21:30:34 +0300

    main http methods were added as staticmethhods

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -2,11 +2,27 @@ import requests
 
 
 class Http_methods:
-
-    headers = {"Content-Type":"application/json"}
-    cookies= ""
-
+    headers = {"Content-Type": "application/json"}
+    cookies = ""
 
     @staticmethod
     def get(url):
-        request = requests.get()
+        request = requests.get(url=url, headers=Http_methods.headers, cookies=Http_methods.cookies)
+        return request
+
+    @staticmethod
+    def post(url, json):
+        request = requests.post(url=url, headers=Http_methods.headers, cookies=Http_methods.cookies, json=json)
+        return request
+
+
+    @staticmethod
+    def put(url, json):
+        request = requests.put(url=url, headers=Http_methods.headers, cookies=Http_methods.cookies, json=json)
+        return request
+
+
+    @staticmethod
+    def delete(url, json):
+        request = requests.delete(url=url, headers=Http_methods.headers, cookies=Http_methods.cookies, json=json)
+        return request
