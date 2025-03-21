deb42fc4b84d18aab61197367c9435a343df46bb
cgpathogen <144326766+cgpathogen@users.noreply.github.com>
2025-03-21 22:26:16 +0300

    the rest methods were added

--- tests/test_user_lifecycle.py
+++ tests/test_user_lifecycle.py
@@ -3,7 +3,10 @@ from utils.api.user import TestUser
 class TestUserLifecycle:
 
     def test_user_lifecycle(self):
+        TestUser.test_send_empty_request()
+        TestUser.test_send_half_request()
         TestUser.test_create_user()
+        TestUser.test_authorization_with_wrong_password()
         TestUser.test_authorization()
         TestUser.test_get_info_about_user()
         TestUser.test_update_user_info()
