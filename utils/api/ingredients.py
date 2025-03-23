7466da1220b0cd4f8639381ba39586b5acd562a8
cgpathogen <144326766+cgpathogen@users.noreply.github.com>
2025-03-23 22:22:48 +0300

    methods test_get_all_ingredients, test_order_by_unauthorized_uer test_place_empty_order added

--- utils/api/ingredients.py
+++ utils/api/ingredients.py
@@ -1,7 +1,39 @@
+import requests
 
+from utils.http_methods import Http_methods
 
 class TestIngredients:
 
     base_url = "https://stellarburgers.nomoreparties.site"
     all_ingredients = "/api/ingredients"
     order = "/api/orders"
+
+
+    @staticmethod
+    def test_get_all_ingredients():
+        used_url = TestIngredients.base_url+TestIngredients.all_ingredients
+        request = requests.get(used_url)
+        assert request.status_code == 200
+        assert request.json()['success'] == True
+        print("Ingredients list received")
+
+
+
+    @staticmethod
+    def test_order_by_unauthorized_uer():
+        used_url = TestIngredients.base_url + TestIngredients.order
+        json = {
+            "ingredients": []
+        }
+        request = requests.post(used_url,json)
+        assert request.status_code == 401
+
+
+    @staticmethod
+    def test_place_empty_order():
+        used_url = TestIngredients.base_url + TestIngredients.order
+        json = {
+            "ingredients": []
+        }
+        request = requests.post(used_url,json)
+        assert request.status_code == 400
