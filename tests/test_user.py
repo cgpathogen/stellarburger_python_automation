a996849342d54cc5f6d590a1ec4c24bfaba731f3
cgpathogen <144326766+cgpathogen@users.noreply.github.com>
2025-03-24 20:13:42 +0300

    test_reauthorization method was added

--- tests/test_user.py
+++ tests/test_user.py
@@ -17,3 +17,10 @@ class TestUserMethods:
     def test_create_existing_user(self):
         TestUser.test_create_user()
         TestUser.test_creating_of_existing_user()
+
+
+    def test_reauthorization(self):
+        TestUser.test_create_user()
+        TestUser.test_authorization()
+        TestUser.test_logout()
+        TestUser.test_authorization()
