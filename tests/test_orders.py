9efd2fe083a6ec7930cb928209f3a431efc54ec5
cgpathogen <144326766+cgpathogen@users.noreply.github.com>
2025-03-24 15:11:28 +0300

    file added

---
+++ tests/test_orders.py
@@ -0,0 +1,19 @@
+from utils.api.user import TestUser
+from utils.api.ingredients import TestIngredients
+
+class TestOrders:
+
+    def test_get_orders_without_authorization(self):
+        TestIngredients.test_get_unauthorized_user_orders()
+
+
+    def test_place_order_without_ingredients(self):
+        TestIngredients.test_place_empty_order()
+
+
+    def test_create_order(self):
+        TestUser.test_create_user()
+        TestUser.test_authorization()
+        TestIngredients.test_get_all_ingredients()
+        TestIngredients.test_place_order()
+        TestIngredients.test_get_authorized_user_orders()
